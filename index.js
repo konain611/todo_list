7ad6c576b440f85b16fe2938d8f5dc2318368eeb
Syed Konain Nasir <konain611@gmail.com>
2024-05-24 14:16:25 +0500

    update
    basic todo list program

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-"use strict";
-console.log("Hello World");
+import inquirer from "inquirer";
+let myList = [];
+let condition = true;
+while (condition) {
+    let options = await inquirer.prompt([{
+            name: "todos",
+            type: "input",
+            message: "What do you want to add in your Todo list?"
+        },
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false",
+        }]);
+    myList.push(options.todos);
+    condition = options.addmore;
+    console.log(myList);
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
 import inquirer from "inquirer";
 
 let myList = [];
+let condition = true;
+
+while(condition){
 
 let options = await inquirer.prompt([{
-    name: ""
+    name: "todos",
+    type: "input",
+    message: "What do you want to add in your Todo list?"
+},
+{
+    name: "addmore",
+    type: "confirm",
+    message: "Do you want to add more?",
+    default: "false",
 }])
+
+
+myList.push(options.todos)
+condition = options.addmore;
+console.log(myList);
+
+}
